255ed3e2769ed42e2e8d911cbb757b5a26d86443
ithomaslin <ithomaslin@gmail.com>
2018-03-16 09:55:17 +0800

    adding cards onClickListener

---
+++ app/src/main/java/com/richify/goobucks/util/CircleTransform.java
@@ -0,0 +1,10 @@
+package com.richify.goobucks.util;
+
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by thomaslin on 14/03/2018.
+ */
+
+public class CircleTransform implements Transformation {
+}
